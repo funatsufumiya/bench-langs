74b42b140618b4c6d8c06cd37287ce95fbaa2870
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-12-14 09:47:03 +0900

    add sh

---
+++ bench.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+# bench.ps1
+# $paths=@(
+# 	".\fibonacci"
+# 	".\tarai"
+# 	".\inc2Tsingle1"
+# 	".\inc2Tsingle2"
+# 	".\inc2Tsingle3"
+# 	".\inc2Tmulti1"
+# 	".\inc2Tmulti2"
+# 	".\inc2Tmulti3"
+# 	".\inc5Tsingle1"
+# 	".\inc5Tsingle2"
+# 	".\inc5Tsingle3"
+# 	".\inc5Tmulti1"
+# 	".\inc5Tmulti2"
+# 	".\inc5Tmulti3"
+# )
+
+# $cmds=@(
+# 	".\vcpp"
+# 	".\clangpp"
+# 	".\ddmd"
+# 	".\dldc"
+# 	".\csharp"
+# 	"csi csx.csx"
+# 	"dotnet cscore\bin\Release\netcoreapp2.0\cscore.dll"
+# 	"mono csmono.exe"
+# 	".\vbnet"
+# 	"dotnet vbcore\bin\Release\netcoreapp2.0\vbcore.dll"
+# 	"mono vbnet.exe"
+# 	"node jsnode.js"
+# 	"node jsnode.min.js"
+# 	"node-chakra jsnode.js"
+# 	"cscript //nologo jswsh.js"
+# 	"cmd /c `"cscript //E:{1b7cd997-e5ff-4932-a7a6-2a9e636da385} //nologo jswsh.js`""
+# 	"cscript //nologo vbswsh.vbs"
+# 	"python py3.py"
+# 	"py -3 py3nb.py"
+# 	"pypy3 py3.py"
+# 	"python ./__pycache__/py3.cpython-36.pyc"
+# 	"py -3 ./__pycache__/py3nb.cpython-36.pyc"
+# 	".\hsp3"
+# 	"sbcl --script cl.lisp"
+# 	"sbcl --script cl.fasl"
+# 	"clisp cl.lisp"
+# 	"clisp cl.fas"
+# 	"wx86cl64 --load ccl.lisp"
+# 	"gosh scm.scm"
+# 	"gosh scmgosh.scm"
+# 	"sash scm.scm"
+# 	"sash scmsash.scm"
+# 	"boot -f clj.clj"	
+# 	"Clojure.Main clj.clj"
+# 	"lumo clj.clj"
+# 	"lumo cljs.cljs"
+# )
+
+# $paths | foreach -Process {
+# 	cd $_
+# 	Write-Host y$_z
+# 	$cmds | foreach -Process {
+# 		Write-host "`n"$_
+# 		for ($i=0; $i -lt 3; $i++){
+# 			Write-host (Measure-Command {iex $_ | Write-host}).TotalMilliseconds
+# 		}
+# 	}
+# 	cd ../
+# }
+
+
+# bench.sh
+
+paths=(
+    "./fibonacci"
+    "./tarai"
+    "./inc2Tsingle1"
+    "./inc2Tsingle2"
+    "./inc2Tsingle3"
+    "./inc2Tmulti1"
+    "./inc2Tmulti2"
+    "./inc2Tmulti3"
+    "./inc5Tsingle1"
+    "./inc5Tsingle2"
+    "./inc5Tsingle3"
+    "./inc5Tmulti1"
+    "./inc5Tmulti2"
+    "./inc5Tmulti3"
+)
+
+langs=(
+    "gcc"
+    "clang"
+    # "nodejs (script)"
+    # "nodejs (min)"
+    "bun (compiled)"
+    "bun (compiled, min)"
+    # "sbcl (script)"
+    # "sbcl (fasl)"
+    # "clisp"
+)
+
+check_files=(
+    "./gpp"
+    "./clangpp"
+    # "jsnode.js" # node (script)
+    # "jsnode.min.js" # node (min)
+    "jsnode_bun" # bun (compiled)
+    "jsnode_min_bun" # bun (compiled, min)
+    # "cl.lisp" # sbcl (script)
+    # "cl.fasl" # sbcl (fasl)
+    # "cl.lisp" # clisp
+)
+
+cmds=(
+    "./gpp" # gcc
+    "./clangpp" # clang
+    # "./ddmd"
+    # "./dldc"
+    # "./csharp"
+    # "csi csx.csx"
+    # "dotnet cscore/bin/Release/netcoreapp2.0/cscore.dll"
+    # "mono csmono.exe"
+    # "./vbnet"
+    # "dotnet vbcore/bin/Release/netcoreapp2.0/vbcore.dll"
+    # "mono vbnet.exe"
+    # "node jsnode.js" # node (script)
+    # "node jsnode.min.js" # node (min)
+    "./jsnode_bun" # bun (compiled)
+    "./jsnode_min_bun" # bun (compiled, min)
+    # "node-chakra jsnode.js"
+    # "cscript //nologo jswsh.js"
+    # "cscript //nologo vbswsh.vbs"
+    # "python py3.py"
+    # "py -3 py3nb.py"
+    # "pypy3 py3.py"
+    # "python ./__pycache__/py3.cpython-36.pyc"
+    # "py -3 ./__pycache__/py3nb.cpython-36.pyc"
+    # "./hsp3"
+    # "sbcl --script cl.lisp" # sbcl (script)
+    # "sbcl --script cl.fasl" # sbcl (fasl)
+    # "clisp cl.lisp" # clisp
+    # "clisp cl.fas"
+    # "wx86cl64 --load ccl.lisp"
+    # "gosh scm.scm"
+    # "gosh scmgosh.scm"
+    # "sash scm.scm"
+    # "sash scmsash.scm"
+    # "boot -f clj.clj"	
+    # "Clojure.Main clj.clj"
+    # "lumo clj.clj"
+    # "lumo cljs.cljs"
+)
+
+# time $(eval $1) shows command not found, suppress it
+function time_bench {
+    start=$(date +%s%N)
+    eval $1
+    end=$(date +%s%N)
+    # line 149: 1734134664N: value too great for base (error token is "1734134664N")
+    # echo "time: " $((end-start)) "ns"
+
+    # this won't show < ms
+    # echo "time: " $(echo "scale=3; ($end - $start) / 1000000" | bc) "ms"
+
+    # remove N from the number
+    start=${start%N}
+    end=${end%N}
+
+    # echo "time: " $(python -c "print(($end - $start) / 1000000.0)") "ms"
+
+    # format print 0.00000 ms
+    # echo "time: " $(python -c "print('{:.5f}'.format(($end - $start) / 1000000.0))") "ms"
+
+    # using decimal
+    echo "time: " $(python -c "from decimal import Decimal; print('{:.5f}'.format((Decimal($end) - Decimal($start)) / Decimal(1000000)))") "ms"
+}
+
+for path in ${paths[@]}; do
+    cd $path
+    echo $path "===================="
+    idx=0
+    cmd_len=${#cmds[@]}
+    # for cmd in ${cmds[@]}; do
+    for i in $(seq 0 $((cmd_len-1))); do
+        cmd=${cmds[$i]}
+        check_file=${check_files[$idx]}
+        lang=${langs[$idx]}
+        if [ ! -f $check_file ]; then
+            # echo "File not found: $check_file"
+            idx=$((idx+1))
+            continue
+        fi
+        # echo "---"  $cmd
+        echo "$lang ----"
+        echo $cmd
+        for i in {1..3}; do
+            # time_bench $cmd
+            time $cmd
+        done
+        idx=$((idx+1))
+    done
+    cd ../
+done
---
+++ build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# build.ps1
+#
+# $paths=@(
+# 	".\fibonacci",
+# 	".\tarai"
+# 	".\inc2Tsingle1"
+# 	".\inc2Tsingle2"
+# 	".\inc2Tsingle3"
+# 	".\inc2Tmulti1"
+# 	".\inc2Tmulti2"
+# 	".\inc2Tmulti3"
+# 	".\inc5Tsingle1"
+# 	".\inc5Tsingle2"
+# 	".\inc5Tsingle3"
+# 	".\inc5Tmulti1"
+# 	".\inc5Tmulti2"
+# 	".\inc5Tmulti3"
+# )
+
+# $cmds=@(
+# 	"cmd /c `"```"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall```" & ```"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\lib\cl```" /EHsc /O2 /openmp vcpp.cpp`"",
+# 	"clang -O3 -o clangpp.exe vcpp.cpp",
+# 	"cmd /c `"dmd -O -release -inline -boundscheck=off -of=ddmd.exe d.d`"",
+# 	"cmd /c `"C:\D\ldc\lib\ldmd2 -O -release -inline -boundscheck=off -of=dldc.exe d.d`"",
+# 	"csc /optimize+ csharp.cs",
+# 	"cmd /c `"dmcs -optimize+ -out:csmono.exe csharp.cs`"",
+# 	"vbc /optimize+ /removeintchecks+ /out:vbnet.exe vbnet.vb",
+# 	"dotnet new console -n cscore --force",
+# 	"cd cscore",
+# 	"cmd /c `"del /q Program.cs`"",
+# 	"cmd /c `"del /q cscore.csproj`"",
+# 	"cmd /c `"copy /y ..\csharp.cs Program.cs`"",
+# 	"cmd /c `"copy /y ..\..\lib\cscore.csproj cscore.csproj`"",
+# 	"dotnet build",
+# 	"cd ../",
+# 	"dotnet new console -lang vb -n vbcore --force",
+# 	"cd vbcore",
+# 	"cmd /c `"del /q Program.vb`"",
+# 	"cmd /c `"del /q vbcore.vbproj`"",
+# 	"cmd /c `"copy /y ..\vbnet.vb Program.vb`"",
+# 	"cmd /c `"copy /y ..\..\lib\vbcore.vbproj vbcore.vbproj`"",
+# 	"dotnet build",
+# 	"cd ../",
+# 	"cmd /c `"uglifyjs -m -o jsnode.min.js jsnode.js`"",
+# 	"python -m compileall py3.py",
+# 	"python -m compileall py3nb.py",
+# 	"sbcl --script ../bin/fasl.lisp cl.lisp",
+# 	"clisp -c cl.lisp"
+# )
+
+# $paths | foreach -Process {
+# 	cd $_
+# 	Write-Host y$_z
+# 	$cmds | foreach -Process {
+# 		Write-host "`n"$_
+# 		iex $_
+# 	}
+# 	cd ../
+# }
+
+
+# build.sh (need to remove .exe or other windows specific extensions)
+ 
+paths=(
+    "./fibonacci"
+    "./tarai"
+    "./inc2Tsingle1"
+    "./inc2Tsingle2"
+    "./inc2Tsingle3"
+    "./inc2Tmulti1"
+    "./inc2Tmulti2"
+    "./inc2Tmulti3"
+    "./inc5Tsingle1"
+    "./inc5Tsingle2"
+    "./inc5Tsingle3"
+    "./inc5Tmulti1"
+    "./inc5Tmulti2"
+    "./inc5Tmulti3"
+)
+
+check_files=(
+    "vcpp.cpp"
+    "vcpp.cpp"
+    "../bin/fasl.lisp"
+    "jsnode.js"
+    "jsnode.min.js"
+)
+
+cmds=(
+    "g++ -O3 -o gpp vcpp.cpp"
+    "clang++ -O3 -o clangpp vcpp.cpp"
+ 	"sbcl --script ../bin/fasl.lisp cl.lisp"
+    # compile with bun"
+    "bun build --compile jsnode.js --outfile jsnode_bun"
+    "bun build --compile jsnode.min.js --outfile jsnode_min_bun"
+)
+
+for path in "${paths[@]}"
+do
+    cd $path
+    echo $path
+    idx=0
+    for cmd in "${cmds[@]}"
+    do
+        check_file=${check_files[$idx]}
+        if [ ! -f $check_file ]; then
+            # echo "File not found: $check_file"
+            idx=$((idx+1))
+            continue
+        fi
+        echo $cmd
+        eval $cmd
+
+        idx=$((idx+1))
+    done
+    cd ../
+done
